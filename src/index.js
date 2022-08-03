c439943c4d689bfc0233f7b23392414951c73dd5
Tim <timcash@gmail.com>
2022-08-03 11:27:54 -0500

    try with new cert

--- src/index.js
+++ src/index.js
@@ -1,28 +1,46 @@
-const { MongoClient } = require("mongodb");
-
-
+// @ts-check
+import { MongoClient } from "mongodb";
+import { readFileSync } from "fs";
 
 // Connection URI
-const uri =
-  "mongodb://soilspec4gg:soilspec4gg@api.soilspectroscopy.org/soilspec4gg?ssl=true";
+const url =
+"mongodb://soilspec4gg:soilspec4gg@api.soilspectroscopy.org:27017/?ssl=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=soilspec4gg&authMechanism=SCRAM-SHA-256"
+// Read the certificate authority
+var ca = readFileSync("./lets-encrypt-r3.pem");
 
-// Create a new MongoClient
-const client = new MongoClient(uri);
-
-async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-
-    // Establish and verify connection
-    const cursor = client.db.collection("AfSIS1").find()
-    cursor.each(function(err, doc) {
-      console.log(doc);
-    });
-    console.log("Connected successfully to server");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+// Database Name
+const dbName = 'soilspec4gg';
+// Connect using MongoClient
+MongoClient.connect(url,{
+  // sslCA : ca,
+},
+   function(err, client) {
+  if (err) {
+    // log th error
+    console.log(err);
+    throw err;
   }
-}
-run().catch(console.dir);
+  const db = client.db(dbName);
+  client.close();
+});
+
+// async function run() {
+//   try {
+//     // Connect the client to the server (optional starting in v4.7)
+//     await client.connect();
+//     console.log("Connected to server");
+
+//     // Establish and verify connection
+//     const cursor = client.db("soilspec4gg").collection("soillab").find();
+//     cursor.forEach(function(doc) {
+//       console.log(doc);
+//     });
+
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
